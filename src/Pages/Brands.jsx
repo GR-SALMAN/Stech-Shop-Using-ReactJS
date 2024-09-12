9cfb8c0ced5d81565aa893999f04d971de98bd81
G.R. Salman <grsalman769@gmail.com>
2024-09-12 22:42:38 +0600

    brands page bug

--- src/Pages/Brands.jsx
+++ src/Pages/Brands.jsx
@@ -1,7 +1,21 @@
 // import React from "react";
 
-const Brands = () => {
-  return <div>Brands</div>;
+import { useContext } from "react";
+import CartMapItems from "./Cart Components/CartMapItems";
+import { ProviderContext } from "../ContextAPI/CartContext";
+const Products = () => {
+  const { GPU } = useContext(ProviderContext);
+  return (
+    <div className="products">
+      <div className="mainPage p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+          {GPU.map((gpu) => (
+            <CartMapItems key={gpu} gpu={gpu} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Brands;
+export default Products;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Navigation from "../Navigation";
 import HomePage from "../Pages/HomePage";
-import Brands from "../Pages/Brands"; // Note the consistent casing
 import AboutUs from "../Pages/AboutUs"; // Corrected casing in import path
 import Contact from "../Pages/Contact";
 import Cart from "../Pages/Cart Components/Cart";
+import Products from "../Pages/Brands";
 export const RootRouter = createBrowserRouter([
   {
     path: "/",
     element: <Navigation />,
     children: [
       { path: "/", element: <HomePage /> },
-      { path: "brands", element: <Brands /> }, // Removed leading '/'
+      { path: "products", element: <Products /> }, // Removed leading '/'
       { path: "about", element: <AboutUs /> }, // Removed leading '/'
       { path: "contact", element: <Contact /> }, // Removed leading '/'
       { path: "cart", element: <Cart /> }, // Removed leading '/'
